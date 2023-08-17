fa6744191417f4e43896be0d72210cfbd43524dc
sgrolon <erickrolon16@gmail.com>
2023-08-17 01:02:53 -0300

    Primeiro Commit

---
+++ Back/src/ProEventos.API/Controllers/Evento.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento{
+                    EventoId = 1,
+                    Tema = "Angular 11 e .NET 5",
+                    Local = "Belo Horizonte",
+                    Lote = "1 LOTE",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                },
+                new Evento{
+                    EventoId = 2,
+                    Tema = "Angular 11 e .NET 5",
+                    Local = "Maringá",
+                    Lote = "2 LOTE",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+                }
+        };
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(Evento => Evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de Put com id: {id}";
+        }
+    }
+}
---
+++ Back/src/ProEventos.API/Models/Evento.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace ProEventos.API.Models
+{
+     public class Evento{
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string Lote { get; set; }
+        public string ImagemURL { get; set; }
+     }
+}

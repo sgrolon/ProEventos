3220a4184b2b260ec925cd0ecb6f2130351ea4ab
sgrolon <erickrolon16@gmail.com>
2023-08-17 02:01:15 -0300

    Seção 3: Introdução ao EF Core

--- Back/src/ProEventos.API/Controllers/Evento.cs
+++ Back/src/ProEventos.API/Controllers/Evento.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,40 +13,23 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[]{
-            new Evento{
-                    EventoId = 1,
-                    Tema = "Angular 11 e .NET 5",
-                    Local = "Belo Horizonte",
-                    Lote = "1 LOTE",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento{
-                    EventoId = 2,
-                    Tema = "Angular 11 e .NET 5",
-                    Local = "Maringá",
-                    Lote = "2 LOTE",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-                }
-        };
-
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
+            _context = context;
 
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(Evento => Evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(Evento => Evento.EventoId == id);
         }
 
         [HttpPost]
---
+++ Back/src/ProEventos.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Data
+{
+    public class DataContext : DbContext
+    {
+        
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
